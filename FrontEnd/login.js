d3dee12a70fd22ba175b05e3458b05d4155a6839
lasquale619 <med.bouguellid@hotmail.com>
2025-07-07 20:38:56 +0200

    Commit 4

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,13 +3,14 @@
  *************************************************************/
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    // on selctionne le formulair
+    // Sélection du formulaire de connexion
     let form=document.querySelector(".login-form");
-    // on ecoute le click de connexion
+     // Écoute de l’événement "submit" du formulaire
     form.addEventListener("submit", (e)=>{
         // on evite le rechargement de la page
-        e.preventDefault();
-        // on recupere l'email et le MDP
+        e.preventDefault(); // Empêche le rechargement de la page
+
+        // Récupération des données saisies par l'utilisateur
         let email = document.querySelector("#email").value;
         let password = document.querySelector("#password").value;
         
@@ -20,22 +21,28 @@ document.addEventListener("DOMContentLoaded", ()=>{
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",  // POST pour envoie des données 
             headers: {
-                "Content-Type": "application/json" // on demande le header en json
+                "Content-Type": "application/json" // Spécifie le type de contenu
             },
-            body: JSON.stringify({ email, password }) // on demande les donneés  on json
+            body: JSON.stringify({ email, password }) // Données envoyées au serveur
         })
+
+         // Analyse de la réponse
         .then(response => {
             if (response.ok){
-                return response.json() // on converti la reponse en json
+                return response.json() // Conversion de la réponse en JSON
             }else {
+                 // Affichage d'un message d'erreur en cas d’échec
                 document.querySelector(".erreurMSG").style.display = "block"; // si la reponse est KO on affiche ce message
             }           
             
-        })               
-        .then(data => { 
-            localStorage.setItem("authToken", data.token); // on le stock ici 
-            window.location.href = "index.html"; // et on ce redirige vers index.html
         })
+        
+        // Traitement des données de réponse (si connexion réussie)        
+        .then(data => { 
+            localStorage.setItem("authToken", data.token); // Stockage du token dans le localStorage 
+            window.location.href = "index.html"; // Redirection vers la page d’accueil
+        })
+        // Gestion des erreurs réseau ou autres
         .catch(error => console.log(error) );
     })
 })
